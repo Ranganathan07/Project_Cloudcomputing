298eded22645eb39d67544b493e5caf0caa27d56
Ranganathan07 <ranguranganathan2006@gmail.com>
2024-12-17 14:12:41 +0530

    Add files via upload

---
+++ 24BSCS241/Project Ranganathan/erp_server/Human_Resources/employee_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Employee Details"
+
+read Employee Details
+
+echo "Emp Code           Emp Name            Emp NO"
+
+echo "0001               Ram                8865467842"
+echo "0002               Raju               7665045654"
+echo "0003               sheela             9192746857"
+
